fb0e09d8a791df5bb98870ddc40b40aa529b6308
SUSHANT CHANDRASHEKHAR MAHALLE <mahallesushant1998@gmail.com>
2021-09-07 16:43:33 +0530

    feat: take input and generate profit/losss

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-cont
+const priceBuyInput=document.querySelector('#priceBuy')
+const quantityInput=document.querySelector('#quantity')
+const priceCurrentInput=document.querySelector('#priceCurrent')
+const btnTellMe=document.querySelector('#btn-tell-me')
+
+const outputAbsolute=document.querySelector("#absolute")
+const outputPercentage=document.querySelector('#percentage')
+btnTellMe.addEventListener('click',()=>
+{
+    const priceBuy=parseFloat(priceBuyInput.value);
+    const quantity=parseInt(quantityInput.value);
+    const priceCurrent=parseFloat(priceCurrentInput.value)
+
+    let resultAbsolute=(priceCurrent-priceBuy)*quantity
+    console.log(resultAbsolute)
+    outputAbsolute.textContent=resultAbsolute
+
+    let resultPercent=(priceCurrent-priceBuy)/(priceBuy/100)
+    console.log(resultPercent)
+    outputPercentage.textContent=resultPercent.toFixed(2)
+})
